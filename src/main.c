232581c0a3cd2a6f56aaa94b17bad63c8fd94f7d
mieksleb <michael.selby@oriel.ox.ac.uk>
2023-08-24 10:52:53 +0100

    debug inf coeff 28

--- src/main.c
+++ src/main.c
@@ -34,22 +34,25 @@ int main() {
 
 	printf(" %lf %lf\n", panelList->data[N-1].pos1.x, panelList->data[N-1].pos1.y);
 
-	// Create all matrices and vectors 
-	double *b = (double *)malloc((N+1) * sizeof(double *));
-    double **A = (double **)malloc((N+1) * sizeof(double *));
-	double **I = (double **)malloc( N * sizeof(double *));
-	double **J = (double **)malloc( N * sizeof(double *));
-	double **K = (double **)malloc( N * sizeof(double *));
-	double **L = (double **)malloc( N * sizeof(double *));
-    for (int i = 0; i < N + 1; i++) {
-        A[i] = (double *)malloc((N+1) * sizeof(double));
-    }
-	for (int i = 0; i < N; i++) {
-        I[i] = (double *)malloc(N * sizeof(double));
-		J[i] = (double *)malloc(N * sizeof(double));
-		K[i] = (double *)malloc(N * sizeof(double));
-		L[i] = (double *)malloc(N * sizeof(double));
-    }
+// Create all matrices and vectors 
+double *b = (double *)malloc((N + 1) * sizeof(double)); // Corrected from double *
+double **A = (double **)malloc((N + 1) * sizeof(double *)); // Corrected from double **
+double **I = (double **)malloc(N * sizeof(double *));
+double **J = (double **)malloc(N * sizeof(double *));
+double **K = (double **)malloc(N * sizeof(double *));
+double **L = (double **)malloc(N * sizeof(double *));
+
+for (int i = 0; i < N + 1; i++) {
+    A[i] = (double *)malloc((N + 1) * sizeof(double));
+}
+
+for (int i = 0; i < N; i++) {
+    I[i] = (double *)malloc(N * sizeof(double));
+    J[i] = (double *)malloc(N * sizeof(double));
+    K[i] = (double *)malloc(N * sizeof(double));
+    L[i] = (double *)malloc(N * sizeof(double));
+}
+
 
 	// Check if memory allocation was successful
 	if (b == NULL || A == NULL || I == NULL || J == NULL || K == NULL || L == NULL) {
