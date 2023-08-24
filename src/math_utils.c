d6f2831f9f78f55d700c50b0b8f2dc7a419d6a0c
mieksleb <michael.selby@oriel.ox.ac.uk>
2023-08-24 10:43:02 +0100

    debug inf coeff 26

--- src/math_utils.c
+++ src/math_utils.c
@@ -43,9 +43,10 @@ void getInfluenceCoefficients (PanelList *list, double **A, double **I, double *
 
         A[i][N] = SUM1;
         printf("%d %lf\n", i, A[i][N]);
+        printf("chuberr\n");
     }
 
-    printf("chuber");
+    printf("chuber\n");
     for (int i = 0; i < N; i++) {
             printf("%d %lf\n", i, A[i][N]);
     }
