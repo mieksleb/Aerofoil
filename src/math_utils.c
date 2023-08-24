025f9953b7bdf82c6ce51030ebbb19f0255cf85b
mieksleb <michael.selby@oriel.ox.ac.uk>
2023-08-24 16:47:31 +0100

    reveretd back to calculating influence coeffs in main

--- src/math_utils.c
+++ src/math_utils.c
@@ -243,6 +243,7 @@ for (int i = 0; i < dim; i++) {
 
     // Forward elimination
     for (int i = 0; i < dim - 1; i++) {
+
         for (int j = i + 1; j < dim; j++) {
             double factor = A_copy[j][i] / A_copy[i][i];
             for (int k = i; k < dim; k++) {
@@ -271,45 +272,8 @@ for (int i = 0; i < dim; i++) {
 
 
 
-double getLiftCoefficient(PanelList *list, double alpha, double V_inf) {
+double getLiftCoefficient(PanelList *list, double *cp, double alpha) {
     int N = list->num_panels; // number of panels
-
-	// Create all matrices and vectors 
-	double *b = (double *)malloc((N + 1) * sizeof(double)); // Corrected from double *
-	double **A = (double **)malloc((N + 1) * sizeof(double *)); // Corrected from double **
-	double **I = (double **)malloc(N * sizeof(double *));
-	double **J = (double **)malloc(N * sizeof(double *));
-	double **K = (double **)malloc(N * sizeof(double *));
-	double **L = (double **)malloc(N * sizeof(double *));
-
-	for (int i = 0; i < N + 1; i++) {
-		A[i] = (double *)malloc((N + 1) * sizeof(double));
-	}
-
-	for (int i = 0; i < N; i++) {
-		I[i] = (double *)malloc(N * sizeof(double));
-		J[i] = (double *)malloc(N * sizeof(double));
-		K[i] = (double *)malloc(N * sizeof(double));
-		L[i] = (double *)malloc(N * sizeof(double));
-	}
-
-
-	// Check if memory allocation was successful
-	if (b == NULL || A == NULL || I == NULL || J == NULL || K == NULL || L == NULL) {
-		printf("Memory allocation failed.\n");
-	}
-
-	// Calculate matrix and vector elements
-	getInfluenceCoefficients (list, A, I, J, K, L, b, V_inf, alpha);
-
- 	// Solve linear system
-	double *x = solveLinearSystem( A, b, N+1);
-
-	// get the pressure coefficients
-	double *cp = (double *)malloc((N+1) * sizeof(double *));
-
-	getPressureCoefficients (list, J, L, cp, x, V_inf, alpha);
-
     double C_lift = 0;
     for (int i = 0; i < N; i++) {
 
@@ -318,17 +282,6 @@ double getLiftCoefficient(PanelList *list, double alpha, double V_inf) {
         C_lift += cp[i] * paneli.len * sin (alpha - beta);
     }
 
-
-    	
-    free(cp);
-	free(A);
-	free(I);
-	free(J);
-	free(K);
-	free(L);
-	free(x);
-	free(b);
-    
     return C_lift;
 
 
